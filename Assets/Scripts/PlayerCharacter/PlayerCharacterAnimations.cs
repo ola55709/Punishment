c4a33d8a7aaf0da23911075494c3bafdc67f0dbf
Aleksandra Janowska <dsw51712@student.dsw.edu.pl>
2025-05-31 20:13:39 +0200

    ininital commit

---
+++ Assets/Scripts/PlayerCharacter/PlayerCharacterAnimations.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class PlayerCharacterAnimations : MonoBehaviour
+{
+    private PlayerCharacterMovment _playerCharacterMovment;
+    private Animator _animator;
+    void Start()
+    {
+        _playerCharacterMovment = GetComponent<PlayerCharacterMovment>();
+        _animator = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        Debug.Log(_playerCharacterMovment.PlayerHorizontalDirection);
+        _animator.SetInteger("Horizontal Speed", (int)_playerCharacterMovment.PlayerHorizontalSpeed);
+        if(_playerCharacterMovment.PlayerHorizontalDirection < 0)
+        {
+            var scale = transform.localScale;
+            scale.x = -Mathf.Abs(transform.localScale.x);
+            transform.localScale = scale;
+        }
+        else if (_playerCharacterMovment.PlayerHorizontalDirection > 0)
+        {
+            var scale = transform.localScale;
+            scale.x = Mathf.Abs(transform.localScale.x);
+            transform.localScale = scale;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerCharacter/PlayerCharacterMovment.cs
@@ -0,0 +1,77 @@
+using Unity.VisualScripting;
+using UnityEditor.ShaderGraph;
+using UnityEngine;
+using UnityEngine.Windows;
+
+public class PlayerCharacterMovment : MonoBehaviour
+{
+    public float PlayerHorizontalSpeed { get; private set; }
+    public int PlayerHorizontalDirection { get; private set; }
+
+    [SerializeField]
+    private float _moveSpeed = 5f;
+    [SerializeField]
+    private float _jumpForce;
+    [SerializeField]
+    private Collider2D _isGroundedColider;
+    [SerializeField]
+    private LayerMask _floorLayerMask;
+    [SerializeField]
+    private float _fallMultiplier = 2.5f;
+
+    private PlayerMovment _controls;
+    private Vector2 _moveInput;
+    private Rigidbody2D _rb;
+
+    private void Awake()
+    {
+        _controls = new PlayerMovment();
+
+        _controls.Player.Move.performed += ctx => _moveInput = ctx.ReadValue<Vector2>();
+        _controls.Player.Move.canceled += ctx => _moveInput = Vector2.zero;
+
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnEnable()
+    {
+        _controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _controls.Disable();
+    }
+
+    private bool IsGrounded()
+    {
+        if (_isGroundedColider != null && _isGroundedColider.IsTouchingLayers(_floorLayerMask)) 
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private void Update()
+    {
+        if (_controls.Player.Jump.WasPressedThisFrame() && IsGrounded())
+        {
+            _rb.linearVelocity = new Vector2(_rb.linearVelocity.x, 0f); // Reset Y velocity
+            _rb.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (_rb.linearVelocity.y < 1)
+        {
+            _rb.linearVelocity += Vector2.up * Physics2D.gravity.y * (_fallMultiplier - 1) * Time.fixedDeltaTime;
+        }
+        _rb.linearVelocity = new Vector2(_moveInput.x * _moveSpeed, _rb.linearVelocityY);
+        PlayerHorizontalSpeed = Mathf.Abs(_rb.linearVelocity.x);
+        PlayerHorizontalDirection = _rb.linearVelocity.x == 0f ? 0 : (int)Mathf.Sign(_rb.linearVelocity.x);
+    }
+}
